07c88ba609863097afce6d2c7ec1cd40c68885a2
Богдан Игнатьев <submitthesatan666@gmail.com>
2017-10-25 01:47:22 +0300

    test

--- src/Ogmudebone/MoskitoPHP/producers/builtin/OnDemandStatsProducer.php
+++ src/Ogmudebone/MoskitoPHP/producers/builtin/OnDemandStatsProducer.php
@@ -8,6 +8,11 @@ use Ogmudebone\MoskitoPHP\producers\MoskitoPHPProducer;
 class OnDemandStatsProducer extends MoskitoPHPProducer
 {
 
+    public function __construct($producerId, $category, $subsystem)
+    {
+        parent::__construct($producerId, $category, $subsystem);
+    }
+
     protected function getMapperId()
     {
         return  Mappers::ON_DEMAND;
